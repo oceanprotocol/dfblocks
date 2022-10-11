148d91f7dcbfd7b9e3f7fc5f7328db7187db15d5
trizin <25263018+trizin@users.noreply.github.com>
2022-10-11 12:02:54 +0000

    Update api call

--- src/blocks/api.rs
+++ src/blocks/api.rs
@@ -18,7 +18,10 @@ pub struct Block {
     number: u64,
 }
 
-fn get_block_number_from_timestamp(chain_id: u64, timestamp: u64) -> u64 {
+pub async fn get_block_number_from_timestamp(
+    chain_id: u64,
+    timestamp: u64,
+) -> Result<u64, Box<dyn std::error::Error>> {
     let query = format!(
         r#"
         {{"query":"query {{
@@ -33,23 +36,17 @@ fn get_block_number_from_timestamp(chain_id: u64, timestamp: u64) -> u64 {
 
     // replace \n with empty string
     let query = query.replace("\n", "");
-
-    println!("query: {}", query);
-
-    let client = reqwest::blocking::Client::new();
+    let client = reqwest::Client::new();
     let res = client
         .post(API_URL)
         .header("Content-Type", "application/json")
         .body(query)
         .send()
-        .unwrap();
+        .await?;
 
-    let body = res.text().unwrap();
-
-    println!("{}", body);
-
-    let response: ApiResponse = serde_json::from_str(&body).unwrap();
-    return response.data.blocks[0].number;
+    let body = res.text().await?;
+    let response: ApiResponse = serde_json::from_str(&body)?;
+    return Ok(response.data.blocks[0].number);
 }
 
 // tests
@@ -57,29 +54,39 @@ fn get_block_number_from_timestamp(chain_id: u64, timestamp: u64) -> u64 {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_get_block_number_from_timestamp_1() {
-        let block_number = get_block_number_from_timestamp(1, 1640000000);
+    #[tokio::test]
+    async fn test_get_block_number_from_timestamp_1() {
+        let block_number = get_block_number_from_timestamp(1, 1640000000)
+            .await
+            .unwrap();
         assert_eq!(block_number, 13841761);
     }
-    #[test]
-    fn test_get_block_number_from_timestamp_56() {
-        let block_number = get_block_number_from_timestamp(56, 1640000000);
+    #[tokio::test]
+    async fn test_get_block_number_from_timestamp_56() {
+        let block_number = get_block_number_from_timestamp(56, 1640000000)
+            .await
+            .unwrap();
         assert_eq!(block_number, 13639035);
     }
-    #[test]
-    fn test_get_block_number_from_timestamp_137() {
-        let block_number = get_block_number_from_timestamp(137, 1640000000);
+    #[tokio::test]
+    async fn test_get_block_number_from_timestamp_137() {
+        let block_number = get_block_number_from_timestamp(137, 1640000000)
+            .await
+            .unwrap();
         assert_eq!(block_number, 22747386);
     }
-    #[test]
-    fn test_get_block_number_from_timestamp_246() {
-        let block_number = get_block_number_from_timestamp(246, 1640000000);
+    #[tokio::test]
+    async fn test_get_block_number_from_timestamp_246() {
+        let block_number = get_block_number_from_timestamp(246, 1640000000)
+            .await
+            .unwrap();
         assert_eq!(block_number, 15429205);
     }
-    #[test]
-    fn test_get_block_number_from_timestamp_1285() {
-        let block_number = get_block_number_from_timestamp(1285, 1640000000);
+    #[tokio::test]
+    async fn test_get_block_number_from_timestamp_1285() {
+        let block_number = get_block_number_from_timestamp(1285, 1640000000)
+            .await
+            .unwrap();
         assert_eq!(block_number, 1126445);
     }
 }
