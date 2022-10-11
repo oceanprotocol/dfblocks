cc8cbd22928d0191901b0d868b218db5acd90fd4
trizin <25263018+trizin@users.noreply.github.com>
2022-10-11 11:25:05 +0000

    Main API

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use dfblocks::blocks::blocks::get_blocks_by_chain;
+
+#[get("/blocks/{chainId}")]
+async fn get_blocks(path: web::Path<u32>) -> impl Responder {
+    let chain_id = path.into_inner();
+    let blocks = get_blocks_by_chain(chain_id);
+    // convert blocks to json array using serde
+    let json = serde_json::to_string(&blocks).unwrap();
+    HttpResponse::Ok().body(json)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(get_blocks))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
