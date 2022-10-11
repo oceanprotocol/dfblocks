fa744cfffb6129417c6341b533dffb2d6336ebc6
trizin <25263018+trizin@users.noreply.github.com>
2022-10-11 12:03:06 +0000

    Update main

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use dfblocks::blocks::blocks::get_blocks_by_chain;
 
 #[get("/blocks/{chainId}")]
-async fn get_blocks(path: web::Path<u32>) -> impl Responder {
+async fn get_blocks(path: web::Path<u64>) -> impl Responder {
     let chain_id = path.into_inner();
-    let blocks = get_blocks_by_chain(chain_id);
+    let blocks = get_blocks_by_chain(chain_id).await;
+    match blocks {
+        Ok(blocks) => {
+            let json = serde_json::to_string(&blocks).unwrap();
+            HttpResponse::Ok().body(json)
+        }
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+    }
     // convert blocks to json array using serde
-    let json = serde_json::to_string(&blocks).unwrap();
-    HttpResponse::Ok().body(json)
 }
 
 #[actix_web::main]
